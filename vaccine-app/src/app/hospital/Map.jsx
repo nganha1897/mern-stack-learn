7e8b5354c99c03e8247573011c9e40c52aa84cf7
Ha Tran <hatran@has-macbook-pro.mynetworksettings.com>
2024-07-19 02:40:11 -0400

    update vaccination project

--- vaccine-app/src/app/hospital/Map.jsx
+++ vaccine-app/src/app/hospital/Map.jsx
@@ -3,16 +3,17 @@ import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
 import "mapbox-gl/dist/mapbox-gl.css";
 import axios from 'axios';
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoibmdhbmhhMTg5NyIsImEiOiJjbHlldDkwNG8wNWs0MmtweDFzNzNkcWkwIn0.ZqfdHFVveW99GvmUMs0jdA";
-
 const Map = ({ address }) => {
+  const mapbox_key = import.meta.env.VITE_MAPBOX_API;
+  const geocode_api = import.meta.env.VITE_GEOCODE_API;
+  console.log(mapbox_key);
+
+  mapboxgl.accessToken = mapbox_key;
   const mapContainerRef = useRef(null);
   const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
 
   const geocodeAddress = async (address) => {
-    const apiKey = "bOHwWLJK5MfZRvkAhRoipV45jCrLWqrM";
-    const url = `http://www.mapquestapi.com/geocoding/v1/address?key=${apiKey}&location=${encodeURIComponent(address)}`;
+    const url = `http://www.mapquestapi.com/geocoding/v1/address?key=${geocode_api}&location=${encodeURIComponent(address)}`;
     try {
       const response = await axios.get(url);
       const { results } = response.data;
